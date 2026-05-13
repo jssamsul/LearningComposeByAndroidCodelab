cc916f223e1c8e53b7ab74f07b9321d876bb1ba8
Samsul Huq <samsul@geidea.net>
2026-05-13 14:15:28 +0400

    TextField changes added
    Included minimum height and full width.
    Updated the TextField colors to use the surface color for both focused and unfocused states.

--- BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -21,10 +21,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -49,14 +52,18 @@ fun SearchBar(
 ) {
     TextField(value = "",
               onValueChange = {},
-              modifier = modifier,
+              modifier = modifier.fillMaxWidth()
+                  .heightIn(min = 56.dp),
               placeholder = {
                   Text(stringResource(R.string.placeholder_search))
               },
               leadingIcon = {
                   Icon(imageVector = Icons.Default.Search, contentDescription = null)
               },
-              colors = TextFieldDefaults.colors())
+              colors = TextFieldDefaults.colors(
+                  unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                  focusedContainerColor = MaterialTheme.colorScheme.surface
+              ))
 }
 
 // Step: Align your body - Alignment
