f832f45fca874b137bc6874e121f7b793a0a5e99
Samsul Huq <samsul@geidea.net>
2026-05-14 19:17:20 +0400

    NavigationBar
    Implemented HomeScreen scrolling and SootheBottomNavigation
    
    - Enabled vertical scrolling in `HomeScreen` using `verticalScroll` and `rememberScrollState`.
    - Added `Spacer` elements to `HomeScreen` for improved layout spacing.
    - Implemented `SootheBottomNavigation` using `NavigationBar` and `NavigationBarItem` with Home and Profile destinations.
    - Updated `HomeSection` title padding for better formatting.
    - Enabled the preview for `SootheBottomNavigation`.

--- BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -38,11 +39,18 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Spa
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -183,8 +191,7 @@ fun HomeSection(
                 stringResource(text).uppercase(),
                 modifier = modifier
                     .paddingFromBaseline(
-                            top = 40.dp,
-                            bottom = 16.dp
+                            top = 40.dp, bottom = 16.dp
                     )
                     .padding(horizontal = 16.dp),
                 style = MaterialTheme.typography.titleMedium
@@ -196,7 +203,10 @@ fun HomeSection(
 // Step: Home screen - Scrolling
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(vertical = 16.dp)) {
+    Column(
+            modifier = modifier.verticalScroll(rememberScrollState())
+    ) {
+        Spacer(modifier = modifier.height(16.dp))
         SearchBar(modifier.padding(horizontal = 16.dp))
         HomeSection(R.string.align_your_body) {
             AlignYourBodyRow()
@@ -204,14 +214,29 @@ fun HomeScreen(modifier: Modifier = Modifier) {
         HomeSection(R.string.favorite_collections) {
             FavoriteCollectionsGrid()
         }
+        Spacer(modifier = modifier.height(16.dp))
+        SootheBottomNavigation(modifier)
+        Spacer(modifier = modifier.height(16.dp))
     }
 }
 
 // Step: Bottom navigation - Material
 @Composable
 private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
-    // Implement composable here
+    NavigationBar(modifier) {
+        NavigationBarItem(selected = true, onClick = {}, icon = {
+            Icon(imageVector = Icons.Default.Spa, contentDescription = null)
+        }, label = {
+            Text(stringResource(R.string.bottom_navigation_home))
+        })
+        NavigationBarItem(selected = false, onClick = {}, icon = {
+            Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null)
+        }, label = {
+            Text(stringResource(R.string.bottom_navigation_profile))
+        })
+    }
 }
+
 
 // Step: MySoothe App - Scaffold
 @Composable
@@ -318,7 +343,7 @@ fun ScreenContentPreview() {
     MySootheTheme { HomeScreen() }
 }
 
-//@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
 @Composable
 fun BottomNavigationPreview() {
     MySootheTheme { SootheBottomNavigation(Modifier.padding(top = 24.dp)) }
